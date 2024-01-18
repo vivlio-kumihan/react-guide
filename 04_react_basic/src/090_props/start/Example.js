3502bc39dbc1cee2a7d8f01fa29749333566672c
Nobuyuki Takahiro <vivlio@kumihan.com>
2024-01-18 14:37:38 +0900

    How to pass values in props

--- 04_react_basic/src/090_props/start/Example.js
+++ 04_react_basic/src/090_props/start/Example.js
@@ -1,5 +1,32 @@
 import Child from "./components/Child";
 
-const Example = () => <Child />;
+const Example = () => {
+  const num = 123;
+  const greet = (name) => `hello ${name}!`;
+  const obj = {
+    name: "takahiro",
+    age: 58
+  };
+  const otherObj = {
+    pid: 12345,
+    price: 5000
+  };
+
+  return (
+    <>
+      <Child 
+        num={num}
+        greet={greet}
+        bool
+        // obj={{myName: "takahiro", age: 58}}
+        obj={obj}
+
+        // pid={otherObj.pid}
+        // price={otherObj.price}
+        {...otherObj}
+      />
+    </>
+  );
+};
 
 export default Example;
--- 04_react_basic/src/090_props/start/components/Child.js
+++ 04_react_basic/src/090_props/start/components/Child.js
@@ -1,14 +1,24 @@
-/* POINT 式と文
-式：何らかの値を返すもの（変数に代入できるもの）
-文：変数宣言、for文、if文、switch文やセミコロンで区切るもの。
-*/
-
 import "./Child.css";
 
-const Child = () => {
+const Child = ({
+    color: df = "green",
+    num,      //=> 数値
+    greet,      //=> 関数
+    bool,     //=> 真偽値
+    obj,      //=> 数値
+    pid,      //=> 数値
+    price     //=> 数値
+  }) => {
   return (
-    <div className="component">
+    <div className={`component ${df}`}>
       <h3>Hello Component</h3>
+      <h3>{num}</h3>
+      <h3>{greet("Takahiro")}</h3>
+      <h3>{bool ? "True" : "False"}</h3>
+      <h3>{obj.myName}</h3>
+      <h3>{obj.age}</h3>
+      <h3>{pid}</h3>
+      <h3>{price}</h3>
     </div>
   );
 };
