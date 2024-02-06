acf16dfb37d9f3dfe435537e8978e970e3c44a45
Nobuyuki Takahiro <vivlio@kumihan.com>
2024-02-06 18:01:16 +0900

    study children

--- 04_react_basic/src/110_props_children/end/components/Container.js
+++ 04_react_basic/src/110_props_children/end/components/Container.js
@@ -7,9 +7,9 @@ const Container = ({ title, children, first, second, color }) => {
   return (
     <div className={`container ${color}`}>
       <h3>{title}</h3>
-      <div>{children}</div>
-      <div>{first}</div>
+      {/* <div>{children}</div> */}
       <div>{second}</div>
+      <div>{first}</div>
     </div>
   );
 };
--- 04_react_basic/src/110_props_children/start/components/Container.js
+++ 04_react_basic/src/110_props_children/start/components/Container.js
@@ -1,10 +1,12 @@
 import "./Container.css";
 
-const Container = ({ title, children }) => {
+const Container = ({ title, children, first, second }) => {
   return (
     <div className="container">
       <h3>{title}</h3>
       {children}
+      <div>{first}</div>
+      <div>{second}</div>
     </div>
   );
 };
