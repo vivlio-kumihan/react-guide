e90a133e6f5c9ee4d89439696feee37f339c9d20
Nobuyuki Takahiro <vivlio@kumihan.com>
2024-05-26 06:00:00 +0900

    review set and get elements

--- async_product_info_lesson/memo_dom.md
+++ async_product_info_lesson/memo_dom.md
@@ -378,3 +378,50 @@ document.querySelector(".inner-html").prepend(targetNewElement);
 
 __async_product_info_templateディレクトリのコードを参照__
 
+cloneを使った配列データの展開
+getAttribute関連まとめる
+add, remove, toggle, containsをまとめる
+
+
+```js
+const me = document.querySelector("#me");
+console.log(me.children);
+console.log(me.firstElementChild);
+console.log(me.lastElementChild);
+console.log(me.nextElementSibling);
+console.log(me.previousElementSibling);
+console.log(me.previousElementSibling);
+console.log(me.parentElement);
+console.log(me.nextElementSibling.firstElementChild);
+console.log(me.previousElementSibling.lastElementChild);
+
+const mainTitle = document.querySelector("#main-title");
+mainTitle.textContent = "タイトル";
+
+const subTitle = document.querySelector(".sub-title");
+subTitle.innerHTML = "<strong>サブタイトル</strong>"
+
+const childrenElems = document.querySelectorAll(".child");
+const orderThree = document.querySelector(".order-3");
+
+for(const child of childrenElems) {
+  if (child.classList.contains("order-1")) {
+    orderThree.after(child);
+  } else if (child.classList.contains("order-2")) {
+    const clone = child.cloneNode(true);
+    me.prepend(clone);
+  }
+  child.style.color = child.dataset.color;
+}
+
+const meRect = me.getBoundingClientRect();
+const commentBody = document.querySelector("#comment-body");
+commentBody.prepend(`
+  #meのborderの上端とHTMLの上端の間隔は、${ me.offsetTop }pxです。
+  #meのborderの左端とHTMLの左端の間隔は、${ me.offsetLeft }pxです。
+  ビュー・ポートの上端から#meの枠線の上端までの間隔は、${ meRect.y }pxです。
+  ビュー・ポートの左端から#meの枠線の左端までの間隔は、${ meRect.x }pxです。
+  #meのborderを含めた横幅は、${ me.offsetWidth }pxです。
+  #meのborderを含めた高さは、${ me.offsetHeight }pxです。
+`)
+```
--- async_product_info_lesson/scripts/main.js
+++ async_product_info_lesson/scripts/main.js
@@ -1,38 +1,41 @@
-// ターゲットになる要素のDOM取得
-const cElm = document.querySelector(".create-element");
-// 新規で作成するDOM
-const span = document.createElement("span");
-span.textContent = "Hello, PHP!";
-// ターゲットのDOMに差し込む
-cElm.append(span);
+const me = document.querySelector("#me");
+console.log(me.children);
+console.log(me.firstElementChild);
+console.log(me.lastElementChild);
+console.log(me.nextElementSibling);
+console.log(me.previousElementSibling);
+console.log(me.previousElementSibling);
+console.log(me.parentElement);
+console.log(me.nextElementSibling.firstElementChild);
+console.log(me.previousElementSibling.lastElementChild);
 
-// 埋め込むテンプレートリテラルを作成する。
-const contents = `
-  <article id="article">
-    <h1 id="article__title">記事のタイトル</h1>
-    <div class="article__tag-erea">
-      <span>タグ：</span>
-      <span>スポーツ</span>
-      <span>野球</span>
-      <span>阪神</span>
-    </div>
-    <div class="article__body">記事の本文</div>
-    <div id="recommend" class="article__recommend">
-      <h2>おすすめの記事</h2>
-      <a href="#">他の記事</a>
-    </div>
-  </article>
-`;
+const mainTitle = document.querySelector("#main-title");
+mainTitle.textContent = "タイトル";
 
-// 入れ替え容器になるDOMを生成。
-// DOMに中身を入れて、側を外して中身だけ返す関数を定義する。
-function htmlStrToElement(htmlStr) {
-  const div = document.createElement("div");
-  div.innerHTML = htmlStr;
-  return div.firstElementChild;
+const subTitle = document.querySelector(".sub-title");
+subTitle.innerHTML = "<strong>サブタイトル</strong>"
+
+const childrenElems = document.querySelectorAll(".child");
+const orderThree = document.querySelector(".order-3");
+
+for(const child of childrenElems) {
+  if (child.classList.contains("order-1")) {
+    orderThree.after(child);
+  } else if (child.classList.contains("order-2")) {
+    const clone = child.cloneNode(true);
+    me.prepend(clone);
+  }
+  child.style.color = child.dataset.color;
 }
 
-// htmlStrToElement()関数の引数にテンプレートリテラルを送り
-// ターゲットになる要素へ注ぐ。
-const targetNewElement = htmlStrToElement(contents);
-document.querySelector(".inner-html").prepend(targetNewElement);
+const meRect = me.getBoundingClientRect();
+const commentBody = document.querySelector("#comment-body");
+commentBody.prepend(`
+  #meのborderの上端とHTMLの上端の間隔は、${ me.offsetTop }pxです。
+  #meのborderの左端とHTMLの左端の間隔は、${ me.offsetLeft }pxです。
+  ビュー・ポートの上端から#meの枠線の上端までの間隔は、${ meRect.y }pxです。
+  ビュー・ポートの左端から#meの枠線の左端までの間隔は、${ meRect.x }pxです。
+  #meのborderを含めた横幅は、${ me.offsetWidth }pxです。
+  #meのborderを含めた高さは、${ me.offsetHeight }pxです。
+`)
+
